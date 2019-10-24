f2431052bb96106e06e46370589783f1fb1b3c2f
Stef Marais <stephan3m@gmail.com>
2019-10-24 14:41:59 +0800

    Fixed tracklisting error

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from '../product.service'
+import { ProductService } from '../product.service'
 import { Album } from '../album'
 
 @Component({
